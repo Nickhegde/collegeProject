7bd5cf081d6938a0a0568d9dfcd7116f8e2db1f9
Apoorva Kamat <apoorvaKamat885@outlook.com>
2024-05-26 10:47:24 -0500

    card modification
    PS i did something bad
    ill fix it

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,6 +4,7 @@ import './card.scss';
 export default function Card({cardItem, cardIndex,  onShowModal}) {
   return (
     <div id='card-container' className='card-container' onClick={()=>{onShowModal(cardIndex)}}>
+      <img src={cardItem.bgImg} alt={'abc'} className="card-image" />
       <div className='card-title'>{cardItem.title}</div>
       <div className='card-description'>{cardItem.description}</div>
     </div>
