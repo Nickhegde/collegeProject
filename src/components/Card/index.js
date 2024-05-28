a9bdb3a2e0de60b3d34200b38bdeb29b771cf829
Apoorva Kamat <apoorvaKamat885@outlook.com>
2024-05-27 21:55:31 -0500

    css changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './card.scss';
+import React, { useState, useEffect } from "react";
+import "./card.scss";
 
-export default function Card({cardItem, cardIndex,  onShowModal}) {
+export default function Card({ cardItem, cardIndex, onShowModal }) {
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShrunk(true);
+    }, 1000); // 5000 milliseconds = 5 seconds
+
+    return () => clearTimeout(timer); 
+  }, []);
   return (
-    <div id='card-container' className='card-container' onClick={()=>{onShowModal(cardIndex)}} style={{backgroundImage : `url(${cardItem.bgImg})`}}>
-      <div className='card-title'>{cardItem.title}</div>
-      <div className='card-description'>{cardItem.description}</div>
+    <div
+    id="card-container"
+    className={`card-container ${isShrunk ? 'shrink' : ''}`}
+    onClick={() => { onShowModal(cardIndex); }}
+  >
+    <div className="background-image" style={{ backgroundImage: `url(${cardItem.bgImg})` }}></div>
+    <div className={`card-content ${isShrunk ? 'grow' : ''}`}>
+    <div className="card-title">{cardItem.title}</div>
+    {isShrunk?<ul>
+              {cardItem.description.map((detail, idx) => (
+                <li key={idx}><strong>{detail.topic}:</strong> {detail.details}</li>
+              ))}
+            </ul>:''}
+    
+          
     </div>
-  )
+  </div>
+  );
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,50 +1,88 @@
-import React,{ useState } from 'react';
-import { Carousal, Modal } from 'components';
-import { VALUES } from 'consts';
-import './Home.scss';
-
+import React, { useState, useEffect } from "react";
+import { Carousal, Modal } from "components";
+import { VALUES } from "consts";
+import "./Home.scss";
 
 export default function Home() {
-    const [cardItems, setCardItems] = useState(VALUES.CAROUSAL_ITEMS);
-    const [marginLeftValue, setMarginLeftValue] = useState(0);
-    const [showModal, setShowModal] = useState(false);
-    const [modalContentIndex, setModalContentIndex] = useState(-1);
+  const [cardItems, setCardItems] = useState(VALUES.CAROUSAL_ITEMS);
+  const [marginLeftValue, setMarginLeftValue] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContentIndex, setModalContentIndex] = useState(-1);
 
-    const onLeftScrollClick = () => {
-      setMarginLeftValue(marginLeftValue+100);
+  const onLeftScrollClick = () => {
+    setMarginLeftValue(marginLeftValue + 75);
+  };
+
+  const onRighttScrollClick = () => {
+    setMarginLeftValue(marginLeftValue - 75);
+  };
+
+  const onShowModalToggle = (index = -1) => {
+    setModalContentIndex(index);
+   
+    setShowModal(!showModal);
+  };
+
+  const onSetModalContent = (direction) => {
+    let currentIndex = modalContentIndex;
+    if (direction === "left" && currentIndex > 0) {
+      currentIndex = currentIndex - 1;
+    } else if (direction === "right" && currentIndex < cardItems.length - 1) {
+      currentIndex = currentIndex + 1;
     }
+    setModalContentIndex(currentIndex);
+  };
 
-    const onRighttScrollClick = () => {
-      setMarginLeftValue(marginLeftValue-100);
-    }
-
-    const onShowModalToggle = (index=-1) => {
-      setModalContentIndex(index);
-      setShowModal(!showModal);
-    }
-
-    const onSetModalContent = (direction) => {
-      let currentIndex = modalContentIndex;
-      if(direction === "left" && currentIndex>0){
-        currentIndex = currentIndex - 1;
-      } else if(direction === "right" && currentIndex<cardItems.length-1) {
-        currentIndex = currentIndex + 1;
-      }
-      setModalContentIndex(currentIndex)
-    }
-
-    let isHideRightScroll= (cardItems.length>3 && (((cardItems.length*100/3)-100)>Math.abs(marginLeftValue))) ? "visible":"hidden";
-    let isHideLeftScroll = marginLeftValue < 0 ? "visible":"hidden";
-    let widthValue = (Math.floor(cardItems.length/3)+1) * 100;
-    let isShowModalLeft = modalContentIndex > 0? "visible":"hidden";
-    let isShowModalRight = modalContentIndex < cardItems.length-1? "visible":"hidden";
+  let isHideRightScroll =
+    cardItems.length > 3 &&
+    (cardItems.length * 100) / 3 - 100 > Math.abs(marginLeftValue)
+      ? "visible"
+      : "hidden";
+  let isHideLeftScroll = marginLeftValue < 0 ? "visible" : "hidden";
+  let widthValue = (Math.floor(cardItems.length / 3) +0.5) * 100;
+  let isShowModalLeft = modalContentIndex > 0 ? "visible" : "hidden";
+  let isShowModalRight =
+    modalContentIndex < cardItems.length - 1 ? "visible" : "hidden";
 
   return (
-    <div id='home-container' className='home-container'>
-      {showModal ? <Modal modalContent={cardItems[modalContentIndex]} onSetModalContent={onSetModalContent} onCloseModal={onShowModalToggle} isShowModalLeft={isShowModalLeft} isShowModalRight={isShowModalRight}></Modal> : null}
-      <div id="left-scroll-button" onClick={onLeftScrollClick} style={{visibility: isHideLeftScroll}}>{`<`}</div>
-      <Carousal Items = {cardItems} marginLeftValue={marginLeftValue} widthValue={widthValue} onShowModal={onShowModalToggle}></Carousal>
-      <div id="right-scroll-button" onClick={onRighttScrollClick} style={{visibility: isHideRightScroll}}>{`>`}</div>
-    </div>
-  )
+    <div
+      id="home-container"
+      className={`home-container ${showModal ? "show" : ""}`}
+    >
+      <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
+        <h1 id='app-title'>Chronicles of Technological Transformation</h1>
+      </div>
+      <div className="app-content">
+      {showModal ? (
+        <Modal
+          modalContent={cardItems[modalContentIndex]}
+          onSetModalContent={onSetModalContent}
+          onCloseModal={onShowModalToggle}
+          isShowModalLeft={isShowModalLeft}
+          isShowModalRight={isShowModalRight}
+        ></Modal>
+      ) : null}
+      <div
+        id="left-scroll-button"
+        onClick={onLeftScrollClick}
+        style={{ visibility: isHideLeftScroll }}
+      >{`<`}</div>
+      {showModal ? (
+        ""
+      ) : (
+        <Carousal
+          Items={cardItems}
+          marginLeftValue={marginLeftValue}
+          widthValue={widthValue}
+          onShowModal={onShowModalToggle}
+        ></Carousal>
+      )}
+      <div
+        id="right-scroll-button"
+        onClick={onRighttScrollClick}
+        style={{ visibility: isHideRightScroll }}
+      >{`>`}</div>
+      </div>
+     </div>
+  );
 }
